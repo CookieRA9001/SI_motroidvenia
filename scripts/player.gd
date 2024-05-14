0d543e55327455aad10b8513636c5ac049b1eecb
RalfsAizsils <ralfs.aizsils@edu.rtu.lv>
2024-05-14 10:14:45 +0300

    controlls

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,10 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("swap"):
+		swap_friends()
+	if Input.is_action_just_pressed("throw"):
+		throw_friends()
 
 	var direction = Input.get_axis("move_left", "move_right")
 	
@@ -26,3 +30,11 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func swap_friends():
+	# TODO: swap between near friendlies
+	pass
+	
+func throw_friends():
+	# TODO: throw held friendly
+	pass
