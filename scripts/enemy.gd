a229be5d506eebb078289c2339c635b3f4650a4a
Paira <101932096+Pairusa@users.noreply.github.com>
2024-05-18 21:23:46 +0300

    k

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,9 +5,13 @@ enum EnemyStates { IDLE, MOVE, ATTACK }
 var direction = -1
 var enemy_status:EnemyStates = EnemyStates.IDLE
 var rome_states = [EnemyStates.IDLE, EnemyStates.MOVE, EnemyStates.MOVE]
+@onready var animation_timer = $effects/animationTimer
+@onready var effects = $effects
+@onready var currentHealth: int = maxHealth
+@export var knockbackPower: int = 100
 @export var speed = 60
 @export var attack_dash = 200
-
+@export var maxHealth = 2
 @onready var ray_cast_r = $RayCastR
 @onready var ray_cast_l = $RayCastL
 @onready var animated_sprite = $AnimatedSprite2D
@@ -17,7 +21,7 @@ var rome_states = [EnemyStates.IDLE, EnemyStates.MOVE, EnemyStates.MOVE]
 @onready var timer_rome = $TimerRome
 @onready var attack_ray_cast_r = $hitBox/RayCastR
 @onready var attack_ray_cast_l = $hitBox/RayCastL
-
+var allyCollisions = []
 func _ready():
 	hitbox_collision_shape.disabled = true
 
@@ -70,3 +74,25 @@ func attack():
 	hitbox_collision_shape.disabled = true
 	enemy_status = EnemyStates.IDLE
 	
+func hurtBlink():
+	effects.play("hurtBlink")
+	animation_timer.start()
+	await animation_timer.timeout
+	effects.play("RESET")
+
+func knockback(area:Area2D):
+	var knockbackDirections = (global_position - area.global_position).normalized() * knockbackPower
+	print(knockbackDirections.x)
+	velocity = knockbackDirections
+	
+func _on_hurtbox_area_entered(area):
+	if area == $hitBox: return
+	if area.name == "Projectile":
+		allyCollisions.append(area)
+		currentHealth -= 1
+		if currentHealth <= 0:
+			queue_free()
+			return
+		knockback(area)
+		hurtBlink()
+
