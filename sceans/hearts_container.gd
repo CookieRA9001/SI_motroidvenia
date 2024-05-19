074e4349e8e118c96161097a82cfdc8f517e91e5
Paira <101932096+Pairusa@users.noreply.github.com>
2024-05-19 19:57:40 +0300

    hp

--- sceans/hearts_container.gd
+++ sceans/hearts_container.gd
@@ -14,3 +14,11 @@ func setMaxHearts(max: int):
 		var heart = HeartGuiClass.instantiate()
 		add_child(heart)
 	
+func updateHearts(currentHealth: int):
+	var hearts = get_children()
+	
+	for i in range(currentHealth):
+		hearts[i].update(true)
+		
+	for i in range(currentHealth, hearts.size()):
+		hearts[i].update(false)
